e5a1a21d8690d16ec25313f1b587d8ec5f515737
eee <eee@eee2.ia.pw.edu.pl>
2011-07-06 20:39:03 +0200

    Fixes in pcl_to_scan

--- pcl_to_scan/src/cloud_to_scan.cpp
+++ pcl_to_scan/src/cloud_to_scan.cpp
@@ -80,7 +80,7 @@ private:
     output->angle_increment = M_PI/180.0/2.0;
     output->time_increment = 0.0;
     output->scan_time = 1.0/30.0;
-    output->range_min = 0.45;
+    output->range_min = 0.1;
     output->range_max = 10.0;
     
     uint32_t ranges_size = std::ceil((output->angle_max - output->angle_min) / output->angle_increment);
@@ -91,13 +91,26 @@ private:
   
     visualization_msgs::Marker line_list;
 
+
+    float min_z = 100;
+    float max_z = -100;
+    float min_y = 100;
+    float max_y = -100;
   
+//    NODELET_INFO("New scan...");
     for (PointCloud::const_iterator it = cloud->begin(); it != cloud->end(); ++it)
     {
       const float &x = it->x;
       const float &y = it->y;
       const float &z = it->z;
     
+      if (z < min_z) min_z = z;
+      if (z > max_z) max_z = z;
+
+      if (y < min_y) min_y = y;
+      if (y > max_y) max_y = y;
+
+
     /*    for (uint32_t u = std::max((uint32_t)u_min_, 0U); u < std::min((uint32_t)u_max_, cloud.height - 1); u++)
       for (uint32_t v = 0; v < cloud.width -1; v++)
       {
@@ -113,12 +126,12 @@ private:
         NODELET_DEBUG("rejected for nan in point(%f, %f, %f)\n", x, y, z);
         continue;  
       }
-      if (-y > max_height_ || -y < min_height_)
+      if (z > max_height_ || z < min_height_)
       {
-        NODELET_DEBUG("rejected for height %f not in range (%f, %f)\n", x, min_height_, max_height_);
+        NODELET_DEBUG("rejected for height %f not in range (%f, %f)\n", z, min_height_, max_height_);
         continue;
       }
-      double angle = -atan2(x, z);
+      double angle = atan2(y, x);
       if (angle < output->angle_min || angle > output->angle_max)
       {
         NODELET_DEBUG("rejected for angle %f not in range (%f, %f)\n", angle, output->angle_min, output->angle_max);
@@ -126,13 +139,15 @@ private:
       }
     
       int index = (angle - output->angle_min) / output->angle_increment;
-      //printf ("index xyz( %f %f %f) angle %f index %d\n", x, y, z, angle, index);
-      double range_sq = z*z+x*x;
+  //    NODELET_INFO("index xyz( %f %f %f) angle %f index %d", x, y, z, angle, index);
+      double range_sq = y*y+x*x;
       if (output->ranges[index] * output->ranges[index] > range_sq)
         output->ranges[index] = sqrt(range_sq);
       
       
     }
+
+    NODELET_INFO("Y: %f %f, Z: %f %f", min_y, max_y, min_z, max_z);
 
     line_list.header = cloud->header;
     line_list.header.frame_id = output_frame_id_;
