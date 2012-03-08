36325fea96b10880a834b9b95079d99a50d27dce
eee <eee@eee2.(none)>
2012-03-08 19:11:39 +0100

    Laser avoid adds

--- laser_avoid/src/laser_avoid_node.cpp
+++ laser_avoid/src/laser_avoid_node.cpp
@@ -25,29 +25,33 @@ void laserCallback(const sensor_msgs::LaserScanConstPtr& msg)
     } 
   }
 
+  double min_dist = 0.2;
+  double max_dist = 0.8;
 
 //  double l = (1e5*r_range)/500-100;
 //  double r = (1e5*l_range)/500-100;
-  double l = 75 * r_range - 50;
-  double r = 75 * l_range - 50;
+  double l = (r_range - min_dist) / (max_dist-min_dist);
+  double r = (l_range - min_dist) / (max_dist-min_dist);
 
-  if (l > 50)
-    l = 50;
-  if (r > 50)
-    r = 50;
+  if (l > 1)
+    l = 1;
+  if (r > 1)
+    r = 1;
 
-  vel.linear.x = (r+l)/1e3;
+  vel.linear.x = 0.1 * (r+l);
 
+  double turnrate = 45;
+  double turn = turnrate * (r-l);
+  if(turn > turnrate)
+    turn = turnrate;
+  if(turn < -turnrate)
+    turn = -turnrate;
 
-  double turnrate = (r-l);
-  if(turnrate > 10.0)
-    turnrate = 10.0;
-  if(turnrate < -10.0)
-    turnrate = -10.0;
-
-  vel.angular.z = -turnrate * M_PI/180.0;
+  vel.angular.z = -turn * M_PI/180.0;
 
   ROS_INFO("l: %lf r: %lf x: %lf a: %lf", l_range, r_range, vel.linear.x, vel.angular.z);
+  printf("l: %lf r: %lf x: %lf a: %lf\n", l, r, vel.linear.x, vel.angular.z);
+  fflush(stdout);
 }
 
 int main(int argc, char** argv)
